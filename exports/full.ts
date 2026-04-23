647afb2299f2401e3233e52309539f99c71d5554
kay <609075410@qq.com>
2026-04-24 01:35:07 +0800

    补充full导出

---
+++ exports/full.ts
@@ -0,0 +1,64 @@
+/*
+ Copyright (c) 2020 Xiamen Yaji Software Co., Ltd.
+
+ https://www.cocos.com/
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+ worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+ not use Cocos Creator software for developing other software or tools that's
+ used for developing games. You are not granted to publish, distribute,
+ sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+export * from './base';
+
+export * from './2d';
+export * from './3d';
+export * from './animation';
+export * from './audio';
+export * from './video';
+export * from './webview';
+export * from './tween';
+export * from './tiled-map';
+export * from './profiler';
+export * from './primitive';
+export * from './graphics';
+export * from './mask';
+export * from './rich-text';
+export * from './ui';
+export * from './ui-skew';
+export * from './particle';
+export * from './particle-2d';
+export * from './intersection-2d';
+export * from './affine-transform';
+export * from './sorting';
+export * from './sorting-2d';
+export * from './legacy-pipeline';
+export * from './light-probe';
+export * from './geometry-renderer';
+
+export * from './physics-framework';
+export * from './physics-builtin';
+export * from './physics-2d-framework';
+export * from './physics-2d-builtin';
+export * from './physics-2d-box2d';
+
+export * from './spine';
+export * from './dragon-bones';
+export * from './skeletal-animation';
+
+export * from './gfx-webgl';
+export * from './gfx-webgl2';
